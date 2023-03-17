e9f1c56332c08688e71c36664ff6917c366c4e8d
Miklós Milosevits <milosevitsmilos@gmail.com>
2023-03-17 09:32:02 +0100

    Handlebar stuff

--- server.js
+++ server.js
@@ -1,21 +1,55 @@
+// https://www.youtube.com/watch?v=JbrqxPcuYVc&list=PLurIMwd6GdCi3ssXNAcjZ2l5mYaTfYPhf&index=6
+
 import express from 'express';
 import { engine } from 'express-handlebars';
+//import path from 'path';
 
 const app = express();
 const port = 3000;
 
-app.engine('handlebars', engine({
-    defaultLayout: 'main'
+app.engine('.hbs', engine({
+    defaultLayout: 'main',
+    layoutsDir: 'views/layouts',
+    partialsDir: 'views/partials',
+    extname: '.hbs' //change extention from .handlebars to .hbs
 }));
-app.set('view engine', 'handlebars');
+app.set('view engine', '.hbs');
 
 // Routing
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', {
+        title: 'Home Page',
+        fullName: 'Miklós Milosevits',
+        name: 'Miki',
+        showFullName: false,
+        age: 32,
+        showAge: true,
+        havePen: false,
+        people: [
+            'Andi',
+            'Szandi',
+            'Sanyi',
+            'Lora'
+        ],
+        family: {
+            mom: 'Erika',
+            dad: 'Sanyi',
+            son1: 'Tomi',
+            son2: 'Zsolti'
+        },
+        lists: [
+            {
+                list: [1, 2, 3, 4, 5]
+            },
+            {
+                list: ['a', 'b', 'c', 'd', 'e']
+            }
+        ]
+    });
 });
 
 app.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about', { title: 'About Me' });
 });
 
 app.listen(port, () => {
