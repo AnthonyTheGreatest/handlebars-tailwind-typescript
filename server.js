580c77ca24f72cf73905aaec1438341814d1efd0
Miklós Milosevits <milosevitsmilos@gmail.com>
2023-03-17 12:10:31 +0100

    more handlebars

--- server.js
+++ server.js
@@ -1,18 +1,32 @@
 // https://www.youtube.com/watch?v=JbrqxPcuYVc&list=PLurIMwd6GdCi3ssXNAcjZ2l5mYaTfYPhf&index=6
 
 import express from 'express';
-import { engine } from 'express-handlebars';
+// import { engine } from 'express-handlebars';
+import handlebars from 'express-handlebars';
 //import path from 'path';
 
 const app = express();
 const port = 3000;
 
-app.engine('.hbs', engine({
+const hbs = handlebars.create({
     defaultLayout: 'main',
     layoutsDir: 'views/layouts',
     partialsDir: 'views/partials',
-    extname: '.hbs' //change extention from .handlebars to .hbs
-}));
+    extname: '.hbs',
+    helpers: {
+        addTen: function(value) {
+            return value + 10;
+        }
+    }
+});
+
+app.engine('.hbs', hbs.engine);
+//     engine({
+//     // defaultLayout: 'main',
+//     // layoutsDir: 'views/layouts',
+//     // partialsDir: 'views/partials',
+//     // extname: '.hbs' //change extention from .handlebars to .hbs
+// }));
 app.set('view engine', '.hbs');
 
 // Routing
